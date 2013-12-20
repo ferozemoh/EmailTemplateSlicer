1bf8677e2dac210f0e9d0c2358cd5e8fb46c13bf
Feroze <ferozemoh@gmail.com>
2013-12-20 12:45:04 -0800

    remove src

--- src/slicer.js
+++
@@ -1,7 +0,0 @@
-(function(){
-	window.onload = function(){
-		/*var imageBox = document.createElement("textarea");
-		document.getElementById("body").appendChild(imageBox);
-		imageBox.innerHTML = "drag your image here";*/
-	}
-})();
