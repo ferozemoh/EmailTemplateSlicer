bc5713139fbf8f46554f1e05429d3ffd0d2f0068
Feroze <ferozemoh@gmail.com>
2013-12-20 12:02:40 -0800

    adding slicer code

---
+++ slicer/slicer.js
@@ -0,0 +1,8 @@
+
+<script src="jquery-2.0.3.min.js"></script>
+(function(){
+	window.onload = function(){
+		var para = document.getElementById("para");
+		para.innerHTML = "drag your image here";
+	}
+})();
