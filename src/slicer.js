3450d1ba0019e50b7ae9755d7c8a981bf71c6828
Feroze <ferozemoh@gmail.com>
2013-12-25 18:39:33 -0800

    1. added jquery 2. added bootstrap 3. added initial page

---
+++ src/slicer.js
@@ -0,0 +1,8 @@
+(function(){
+	window.onload = function(){
+		<script
+		/*var imageBox = document.createElement("textarea");
+		document.getElementById("body").appendChild(imageBox);
+		imageBox.innerHTML = "drag your image here";*/
+	}
+})();
