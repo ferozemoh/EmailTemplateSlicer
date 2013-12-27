76e5e61cb37fb0700fb926753c6309a5648fd84b
Feroze <ferozemoh@gmail.com>
2013-12-26 18:37:10 -0800

    canvas now shows border with image

--- src/slicer.js
+++ src/slicer.js
@@ -1,17 +1,20 @@
-	function changeBackdrop(){
-		console.log("in changebackdrop function");
-		var imageBox = $('#imagebox');
-		var imageLink = imageBox.val();
-		var canvas = $('#canvas')[0];
-		var ctx = canvas.getContext("2d");
-		var img = new Image();
-		img.onload = function(){
-			// ctx.moveTo(0,550);
-			// ctx.lineTo(700,550);
-			ctx.canvas.width = img.width;
-			ctx.canvas.height = img.height;
-			ctx.drawImage(img,0,0);
-			// ctx.stroke();
-		}
-		img.src = imageLink;
+function changeBackdrop(){
+	console.log("in changebackdrop function");
+	var imageBox = $('#imagebox');
+	var imageLink = imageBox.val();
+	var canvas = $('#canvas')[0];
+	var ctx = canvas.getContext("2d");
+	var img = new Image();
+	img.onload = function(){
+		// ctx.moveTo(0,550);
+		// ctx.lineTo(700,550);
+		ctx.canvas.width = img.width;
+		ctx.canvas.height = img.height;
+		ctx.canvas.style.border = "1px solid black";
+		ctx.drawImage(img,0,0);
+		// ctx.stroke();
 	}
+	img.src = imageLink;
+}
+
+	
