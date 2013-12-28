11bfe99ad554694fb284f8031271f10137cdbf0f
Feroze <ferozemoh@gmail.com>
2013-12-27 21:52:33 -0800

    added draw line when mouse is clicked

--- src/slicer.js
+++ src/slicer.js
@@ -1,41 +1,35 @@
-var canvas;
-var ctx;
 window.onload = function(){
-	//slicing area
-	canvas = $('#canvas')[0];
-	ctx = canvas.getContext("2d");
-	canvas.addEventListener('mousedown', function(evt){
-		var mousePos = getMousePos(evt);	
-		drawLine(ctx,mousePos);
-	},false);
-};
+	var tool = new paper.Tool();
+	
+	//draw a new line on template when clicked
+	tool.onMouseDown = function(event){
+		var point = new paper.Point(0,event.point.y);
+		var canvas = $('#myCanvas')[0]; 
+		var size = new paper.Size(1100, 2);
+		var path = new paper.Path.Rectangle(point, size);
+		path.strokeColor = 'red';
+		path.fillColor = 'red';
+		paper.view.draw();
+	}
+}
 
+//change the background of the canvas to the given image 
 function changeBackdrop(){
-	console.log("in changebackdrop function");
-	var imageBox = $('#imagebox');
+	
+	var canvas = $('#myCanvas')[0];
+	paper.setup(canvas);
+	//setting up canvas with paper
+
+	var imageBox = $('#imagebox')
 	var imageLink = imageBox.val();
 	var img = new Image();
-	img.onload = function(){
-		ctx.canvas.width = img.width; //changing height and width of canvas to that of image
-		ctx.canvas.height = img.height;
-		ctx.canvas.style.border = "1px solid black";
-		ctx.drawImage(img,0,0); //draw given image onto canvas
-	}
+	canvas.style.background = "url(" + imageLink + ")"; //change the background of the canvas
 	img.src = imageLink; //changing image source to the link given in textbox
+
+	//changing height and width of canvas to that of image	
+	canvas.width = img.width;
+	canvas.height = img.height;
+	canvas.style.border = "1px solid black";
+	
 }
 
-function drawLine(ctx,mousePos){
-	console.log('in drawLine');
-		ctx.moveTo(0,mousePos.y)
-		ctx.lineTo(ctx.canvas.width,mousePos.y);
-	ctx.stroke();
-}
-
-// returns the coordinates of the mouse position on the canvas
-function getMousePos(evt){
-	var rect = canvas.getBoundingClientRect();
-	return{
-		x: evt.clientX - rect.left,
-		y: evt.clientY - rect.top
-	};
-}
