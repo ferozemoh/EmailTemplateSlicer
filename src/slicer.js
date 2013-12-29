e2badf7956ba0106804d120cce2acf2b5ccaa1a6
Feroze <ferozemoh@gmail.com>
2013-12-29 14:04:38 -0800

    modified readme

--- src/slicer.js
+++ src/slicer.js
@@ -32,35 +32,39 @@ window.onload = function(){
 			}	
 			if(draw){
 				//draw the line since it doesn't exist
+				var canvas = $('#myCanvas')[0];
 				var point = new paper.Point(0,event.point.y);
-				var size = new paper.Size(1100, 4);
+				var size = new paper.Size(canvas.width, 4);
 				var path = new paper.Path.Rectangle(point, size);
 				path.strokeColor = 'purple';
 				path.fillColor = 'purple';
 				lines.push(path);
-				paper.view.draw();
 			}
 		}
+		paper.view.draw();
 	}
 }
 
 //change the background of the canvas to the given image 
 function changeBackdrop(){
 	
-	var canvas = $('#myCanvas')[0];
-	paper.setup(canvas);
+	// paper.setup(canvas);
 	//setting up canvas with paper
 
 	var imageBox = $('#imagebox')
 	var imageLink = imageBox.val();
 	var img = new Image();
-	canvas.style.background = "url(" + imageLink + ")"; //change the background of the canvas
 	img.src = imageLink; //changing image source to the link given in textbox
+	img.onload = function(){
+		var canvas = $('#myCanvas')[0];
+		paper.setup(canvas);
 
-	//changing height and width of canvas to that of image	
-	canvas.width = img.width;
-	canvas.height = img.height;
-	canvas.style.border = "1px solid black";
+		//changing height and width of canvas to that of image	
+		canvas.style.background = "url(" + imageLink + ")"; //change the background of the canvas
+		canvas.width = img.width;
+		canvas.height = img.height;
+		canvas.style.border = "1px solid black";
+	}
 	
 }
 
